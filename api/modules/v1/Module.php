bc60c3fa1e4fd9a21c0ca995926d8242fac38fb4
jayantakundu <jayantakundu89@gmail.com>
2015-06-18 00:41:13 +0530

    Initial Commit

--- api/modules/v1/Module.php
+++ api/modules/v1/Module.php
@@ -7,6 +7,7 @@ namespace api\modules\v1;
  * @author jayanta kundu <jayantakundu89@gmail.com>
  * @since 1.0
  */
+
 class Module extends \yii\base\Module
 {
     public $controllerNamespace = 'api\modules\v1\controllers';
