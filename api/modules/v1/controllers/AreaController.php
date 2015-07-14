091924acee386c80355a7443a50d48f95f9ae32c
jayantakundu <jayantakundu89@gmail.com>
2015-07-14 16:02:28 +0530

    add area controller

---
+++ api/modules/v1/controllers/AreaController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace api\modules\v1\controllers;
+
+use Yii;
+use yii\rest\ActiveController;
+use yii\data\ActiveDataProvider;
+
+use yii\db\Query;
+
+
+/**
+ * Area Controller API
+ *
+ * @author jayanta kundu created on 11.06.2015
+ */
+class AreaController extends ActiveController
+{
+    public $modelClass = 'api\modules\v1\models\Area';
+
+    public $serializer = [
+        'class' => 'yii\rest\Serializer',
+        'collectionEnvelope' => 'items',
+    ];
+
+    public function actions()
+    {
+        $actions = parent::actions();
+
+    	unset($actions['index']);
+        // disable the "index" actions
+
+        return $actions;
+
+    }
+
+    /**
+     * @return Areas 
+     * @Note: customize index if you need
+     * @author jayanta kundu modified on 10.06.2015
+     */
+    public function actionIndex(){
+	$perpage = 5;
+	$model = new $this->modelClass;
+        $query = $model->find();
+       
+        if (!empty($_GET) && array_key_exists('city_id', $_GET)) {
+	    $query->where(['city_id'=>$_GET['city_id']]);
+
+	    if(array_key_exists('per-page', $_GET)){ $perpage = $_GET['per-page'];}
+	    if(array_key_exists('region_id', $_GET)){ 
+		 $query->andWhere(['region_id'=>$_GET['region_id']]);
+	    }
+	}else{
+   		throw new \yii\web\HttpException(404, 'city required');
+	}
+        
+	return $this->prepareDataProvider($query,$perpage);
+   }    
+   
+  /**
+   * @return prepareDataProvider
+   * @Note: customize prepareDataProvider if you need
+   * @author jayanta kundu created on 11.06.2015
+   */
+    private function prepareDataProvider($query,$perpage){
+	if($perpage>30){ $perpage =30;}
+	else if($perpage<=0){ $perpage =5;}
+
+	try {  
+		$provider = new ActiveDataProvider([
+		    'query' => $query,
+		    'pagination' => [
+			'pageSize' => $perpage,
+		    ],
+		]);
+      
+	    } catch (Exception $ex) {
+		throw new \yii\web\HttpException(500, 'Internal server error');
+	    }
+	    return $provider;
+    }
+
+       
+
+}
+
+
