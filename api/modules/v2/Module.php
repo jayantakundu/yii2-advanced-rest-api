648b05448a6dd5bb1a1d100526f23e19a72ae338
jayantakundu <jayantakundu89@gamil.com>
2015-07-29 14:57:02 +0530

    v2 module added

---
+++ api/modules/v2/Module.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace api\modules\v2;
+/**
+ * API V2 Module
+ *
+ * @author Jayanta Kundu
+ * @since 1.1
+ */
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'api\modules\v2\controllers';
+
+    public function init()
+    {
+        parent::init();        
+    }
+}
---
+++ api/modules/v2/controllers/AreaController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace api\modules\v2\controllers;
+
+use api\modules\v1\controllers\AreaController as BaseAreaController;
+
+/**
+ * Area Controller V2 API
+ *
+ * @author jayanta kundu created on 23.07.2015
+ */
+class AreaController extends BaseAreaController
+{
+   
+       
+}
+
+
